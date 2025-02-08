db382e0e537d9a86130a35e8cf0687b14432c790
Vishnugopal Rajamanickam <vishnugopal.rajamanickam@kiongroup.com>
2025-02-08 08:01:44 -0600

    Predicate files added.

---
+++ src/main/java/predicate/SimpleTest.java
@@ -0,0 +1,20 @@
+package predicate;
+
+import java.util.function.Predicate;
+
+public class SimpleTest {
+    public static void main(String[] args) {
+        Predicate<Integer> isGreaterThan2 = (n) -> n > 2;
+        Predicate<Integer> isLessThan10 = (n) -> n < 10;
+        System.out.println(isGreaterThan2.test(3));//true
+        System.out.println(isGreaterThan2.test(1));//false
+        System.out.println(isLessThan10.test(6));//true
+        System.out.println(isGreaterThan2.and(isLessThan10).test(8));//true
+        System.out.println(isGreaterThan2.and(isLessThan10).test(80));//false
+
+        Predicate<Integer> isBetween2And10 = isGreaterThan2.and(isLessThan10);
+
+        System.out.println(isBetween2And10.test(8));//true
+        System.out.println(isBetween2And10.test(80));//false
+    }
+}
---
+++ src/test/java/com/udemy/java/test/DriverTestWithPredicateAndConsumer.java
@@ -0,0 +1,50 @@
+package com.udemy.java.test;
+
+import com.udemy.java.supplier.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class DriverTestWithPredicateAndConsumer {
+    private WebDriver driver;
+    @BeforeTest
+    public void setDriver(){
+        this.driver = DriverFactory.getDriver("chrome");
+    }
+
+    @Test
+    public void testDriver(){
+        this.driver.get("https://www.google.com/");
+        List<WebElement> linksInGoogle = this.driver.findElements(By.tagName("a"));
+
+        System.out.println("Before :: " + linksInGoogle.size());
+
+        Predicate <WebElement> isBlank = (e) ->{
+           return e.getText().trim().isEmpty();
+        };
+
+        Consumer<WebElement> printElementText = (e) -> {
+            System.out.println(e.getText());
+        };
+
+        linksInGoogle.removeIf(isBlank);
+
+        System.out.println("After :: " + linksInGoogle.size());
+
+        linksInGoogle.forEach(printElementText);
+
+
+    }
+
+    @AfterTest
+    public void tearDown(){
+        this.driver.quit();
+    }
+}
